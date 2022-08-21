8ca2e30ad4ac19c3f96d672f7edfb844f260c71f
KateOpen <kate.front.jwz@gmail.com>
2022-08-21 20:52:06 +0300

    add project

---
+++ my-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+const newLocal = <img alt={"\u0056\u0075\u0065\u0020\u006c\u006f\u0067\u006f"} src={"\u002e\u002f\u0061\u0073\u0073\u0065\u0074\u0073\u002f\u006c\u006f\u0067\u006f\u002e\u0070\u006e\u0067"}></img>;
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
